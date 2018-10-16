2646898d9cdf657a4da51995ccd5efd88f41e3e1
Sana Rafaqat <sanarafaqat041@gmail.com>
2018-10-16 13:35:30 +0100

    added the randomHole function which brings up random hole divs when prompted

--- js/index.js
+++ js/index.js
@@ -1,28 +1,32 @@
 // Variables to use throughout the game
 var scoreboard = document.querySelector('.score');
-var hole = document.querySelectorAll('.hole');
+var holes = document.querySelectorAll('.Hole');
 var mole = document.querySelectorAll('.Mole');
 var score_show = document.querySelector('score_show');
 var btnClick = document.querySelector('.button');
 var start_screen = document.querySelector('start_screen');
 
+
+
+
 // Variable with Let can have a Block Scope
 let score = 0;
 let timesUp = false;
-let lastHome;
+
 
 //Minimum and Maximum time in the randomTime function
 //Math round returns the number nearest to the value of a number rounded to the nearest integer
 // Function that gives random amount of time between min and max
 // A utility
 function randomTime(min,max) {
-       return Math.round (Math.random() * (max - min) + min);
+       return Math.round(Math.random() * (max - min) + min);
 }
 
-// idx catches function
 // holeRandom picks a random hole , which takes a list of hole
 //getmeadomelement
-  function holeRandom(hole) {
-      var numHole = Math.floor(Math.random()* hole.length);
-      var hole = hole[numHole];
-  } console.log(hole);
+// This function brings up random holes which are a div on the HTML page
+function randomHole(holes) {
+       var numbHole = Math.floor(Math.random() * holes.length);
+       var hole = holes[numbHole];
+       console.log(hole);
+   }
